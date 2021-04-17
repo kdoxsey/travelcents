f26aeb5afc297d848c0ec3d8918490ea18bccc5e
kdoxsey <kdoxse@gmail.com>
2021-04-16 20:59:55 -0400

    added math for change to the alert

--- content.js
+++ content.js
@@ -3,9 +3,4 @@ let totalInt = parseFloat(total.split('').splice(1).join('')).toFixed(2)
 let roundUp = parseInt(Math.ceil(total.split('').splice(1).join('')))
 let change = parseFloat(roundUp - totalInt).toFixed(2)
 
-// alert(`Your Amazon total is ${total}. Would you like to round this purchase up to $${roundUp}.00 and add $${change} to your Travel Cents account?`)
-
-chrome.storage.sync.set({key: total}, function() {
-    // alert(`your amazon total is ${total}`)
-})
-
+chrome.storage.sync.set({'total': total,'totalInt': totalInt, 'roundUp': roundUp, 'change': change})
