50655e9d8614c15028d87570db7490a25095c520
kdoxsey <kdoxse@gmail.com>
2021-04-17 21:28:41 -0400

    condensed functions into one 'calcTotal()'

--- popup.js
+++ popup.js
@@ -1,8 +1,10 @@
 chrome.storage.sync.get(['total', 'totalInt', 'roundUp', 'change'], function(result) {
-    // document.getElementById("test").addEventListener("click", alert(`Hello from popup.js! Your Amazon total is ${result.total}. Would you like to round this purchase up to $${result.roundUp}.00 and add $${result.change} to your Travel Cents account?`));
-    document.getElementById('test').addEventListener("click", function() {
+
+    function calcTotal() { 
         document.getElementById("total").innerHTML = `Amazon total: ${result.total}`
         document.getElementById("change").innerHTML = `Change: $${result.change}`
         document.getElementById("newTotal").innerHTML = `New total: $${result.roundUp}.00`
-    })
+        }
+
+    calcTotal()
 })
