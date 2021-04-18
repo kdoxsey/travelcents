4a9042ed56ac6f9e9ddd3629f2442600d14f7aa5
kdoxsey <kdoxse@gmail.com>
2021-04-17 21:12:37 -0400

    amazon total added to DOM with a button click event

--- popup.js
+++ popup.js
@@ -1,3 +1,6 @@
 chrome.storage.sync.get(['total', 'totalInt', 'roundUp', 'change'], function(result) {
-    alert(`Hello from popup.js! Your Amazon total is ${result.total}. Would you like to round this purchase up to $${result.roundUp}.00 and add $${result.change} to your Travel Cents account?`)
+    // document.getElementById("test").addEventListener("click", alert(`Hello from popup.js! Your Amazon total is ${result.total}. Would you like to round this purchase up to $${result.roundUp}.00 and add $${result.change} to your Travel Cents account?`));
+    document.getElementById('test').addEventListener("click", function() {
+        document.getElementById("total").innerHTML = `Amazon total: ${result.total}`
+    })
 })
