68a08debee2f2610933fa4d1921e04c00d41c085
kdoxsey <kdoxse@gmail.com>
2021-04-17 21:17:39 -0400

    added change and new total to the popup

--- popup.js
+++ popup.js
@@ -2,5 +2,7 @@ chrome.storage.sync.get(['total', 'totalInt', 'roundUp', 'change'], function(res
     // document.getElementById("test").addEventListener("click", alert(`Hello from popup.js! Your Amazon total is ${result.total}. Would you like to round this purchase up to $${result.roundUp}.00 and add $${result.change} to your Travel Cents account?`));
     document.getElementById('test').addEventListener("click", function() {
         document.getElementById("total").innerHTML = `Amazon total: ${result.total}`
+        document.getElementById("change").innerHTML = `Change: $${result.change}`
+        document.getElementById("newTotal").innerHTML = `New total: $${result.roundUp}.00`
     })
 })
