ed8836542d91039207031700abdfaf9036ced295
kdoxsey <kdoxse@gmail.com>
2021-04-18 00:46:40 -0400

    start of confirmation pages

--- popup.js
+++ popup.js
@@ -1,20 +1,27 @@
 chrome.storage.sync.get(['total', 'totalInt', 'roundUp', 'change'], function(result) {
 
     function calcTotal() { 
-        document.getElementById("total").innerHTML = `Current total: ${result.total}`
-        document.getElementById("change").innerHTML = `Change: $${result.change}`
-        document.getElementById("newTotal").innerHTML = `New total: $${result.roundUp}.00`
+        document.getElementById("total").innerHTML = `Checkout Total: ${result.total}`;
+        // document.getElementById("change").innerHTML = `Change: $${result.change}`;
+        document.getElementById("newTotal").innerHTML = `New Checkout Total: $${result.roundUp}.00`;
+    }
+
+    function onClick() {
+        document.getElementById("content").innerHTML = `Add $${result.change} to your account?`;
     }
     
     function createButtons() {
-    var buttons = [`$${result.change}`,`$1.00`,`$3.00`,`Other`];
-    for(i = 0; i < buttons.length; i++) {
-    var button = document.createElement("button");
-    button.innerHTML = buttons[i];
-    document.getElementById("content").appendChild(button);
-  }
+        var buttons = [`$${result.change}`,`$1.00`,`$3.00`,`Other`];
+        for(i = 0; i < buttons.length; i++) {
+            var button = document.createElement("button");
+            button.innerHTML = buttons[i];
+            button.setAttribute("id", buttons[i]);
+            button.setAttribute("class", 'btn');
+            document.getElementById("content").appendChild(button);
+            button.addEventListener("click", onClick)
+        }
     }
 
-    calcTotal()
-    createButtons()
+    calcTotal();
+    createButtons();
 })
