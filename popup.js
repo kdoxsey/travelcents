98af5895a06239f78719719fded5b2fa190fcdc9
kdoxsey <kdoxse@gmail.com>
2021-04-18 04:11:22 -0400

    confirmation and back button work, only displays the 'change' variable and not ,

--- popup.js
+++ popup.js
@@ -1,27 +1,37 @@
 chrome.storage.sync.get(['total', 'totalInt', 'roundUp', 'change'], function(result) {
 
-    function calcTotal() { 
-        document.getElementById("total").innerHTML = `Checkout Total: ${result.total}`;
-        // document.getElementById("change").innerHTML = `Change: $${result.change}`;
-        document.getElementById("newTotal").innerHTML = `New Checkout Total: $${result.roundUp}.00`;
-    }
+    function frontPage() {    
+        function calcTotals() { 
+            document.getElementById("total").innerHTML = `Checkout Total: ${result.total}`;
+            document.getElementById("newTotal").innerHTML = `New Checkout Total: $${result.roundUp}.00`;
+        }
 
-    function onClick() {
-        document.getElementById("content").innerHTML = `Add $${result.change} to your account?`;
-    }
-    
-    function createButtons() {
-        var buttons = [`$${result.change}`,`$1.00`,`$3.00`,`Other`];
-        for(i = 0; i < buttons.length; i++) {
+        function createButtons() {
+            function onClick(event) {
+                function onClickNo() {
+                    frontPage();
+                    document.getElementById("confirmation").innerHTML = ``;
+                }
+                document.getElementById("confirmation").innerHTML = `<p">add $${result.change} to your account?</p><div id="confirm-buttons"><button id="no">no</button><button id="confirm-btn">yes</button></div>`;
+                document.getElementById("total").innerHTML = ``;
+                document.getElementById("newTotal").innerHTML = ``;
+                document.getElementById("buttons").innerHTML = ``;
+                document.getElementById("no").addEventListener("click", onClickNo)
+                console.log(event);
+            }
+            let buttons = [`$${result.change}`,`$1.00`,`$3.00`,`Other`];
+            for(i = 0; i < buttons.length; i++) {
             var button = document.createElement("button");
             button.innerHTML = buttons[i];
             button.setAttribute("id", buttons[i]);
             button.setAttribute("class", 'btn');
-            document.getElementById("content").appendChild(button);
             button.addEventListener("click", onClick)
+            document.getElementById("buttons").appendChild(button);
         }
     }
-
-    calcTotal();
-    createButtons();
+    calcTotals()
+    createButtons()
+}
+    
+    frontPage();
 })
