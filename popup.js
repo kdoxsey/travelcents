a0c13e602a2bfb3959fe1de3305845e243c4d794
kdoxsey <kdoxse@gmail.com>
2021-04-17 21:47:53 -0400

    generated a button with change amount on it

--- popup.js
+++ popup.js
@@ -4,7 +4,15 @@ chrome.storage.sync.get(['total', 'totalInt', 'roundUp', 'change'], function(res
         document.getElementById("total").innerHTML = `Amazon total: ${result.total}`
         document.getElementById("change").innerHTML = `Change: $${result.change}`
         document.getElementById("newTotal").innerHTML = `New total: $${result.roundUp}.00`
-        }
+    }
+    
+    function createButtons() {
+        const btn = document.createElement('BUTTON');
+        // btn.textContent('hello')
+        document.body.appendChild(btn)
+        btn.appendChild(document.createTextNode(`$${result.change}`))
+    }
 
     calcTotal()
+    createButtons()
 })
