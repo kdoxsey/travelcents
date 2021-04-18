a63ba19abc8d6e5ebbfd61e1ce24b933ad3cc612
kdoxsey <kdoxse@gmail.com>
2021-04-17 23:20:10 -0400

    split html into content/footer

--- popup.js
+++ popup.js
@@ -1,17 +1,17 @@
 chrome.storage.sync.get(['total', 'totalInt', 'roundUp', 'change'], function(result) {
 
     function calcTotal() { 
-        document.getElementById("total").innerHTML = `Amazon total: ${result.total}`
+        document.getElementById("total").innerHTML = `Current total: ${result.total}`
         document.getElementById("change").innerHTML = `Change: $${result.change}`
         document.getElementById("newTotal").innerHTML = `New total: $${result.roundUp}.00`
     }
     
     function createButtons() {
-    var buttons = [`$${result.change}`,`$1.00`,`$3.00`,`other`];
+    var buttons = [`$${result.change}`,`$1.00`,`$3.00`,`Other`];
     for(i = 0; i < buttons.length; i++) {
     var button = document.createElement("button");
     button.innerHTML = buttons[i];
-    document.body.appendChild(button);
+    document.getElementById("content").appendChild(button);
   }
     }
 
