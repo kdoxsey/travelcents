9f5dea9faae19243e4525f59c1637902ee38dcb2
kdoxsey <kdoxse@gmail.com>
2021-04-19 11:40:42 -0400

    added button values to confirmation page. 'other' button needs input element added

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 chrome.storage.sync.get(['total', 'totalInt', 'roundUp', 'change'], function(result) {
-    let balance = 0
+    let totalSaved = 0
 
     function frontPage() {    
         function calcTotals() { 
@@ -8,21 +8,32 @@ chrome.storage.sync.get(['total', 'totalInt', 'roundUp', 'change'], function(res
         }
 
         function createButtons() {
-            function onClick(event) {
+            function onClickButton(event) {
                 function onClickNo() {
                     frontPage();
                     document.getElementById("confirmation").innerHTML = ``;
                 }
                 function onClickYes() {
-                    balance += parseFloat(result.change);
-                    console.log(balance)
+                    totalSaved += parseFloat(result.change);
+                    document.getElementById("confirmation").innerHTML = `<h4>You added ${event.target.id} to your account.</h4><button id="ok">Ok</button>`;
+                    console.log(totalSaved)
                 }
-                document.getElementById("confirmation").innerHTML = `<h4 id="confirm-ask">add $${result.change} to your account?</h4><div id="confirm-buttons"><button id="no">no</button><button id="yes">yes</button></div>`;
+
+                // function onClickMyAccount() {
+                //     document.getElementById("confirmation").innerHTML = `Total saved: $${totalSaved}`
+                //     document.getElementById("total").innerHTML = ``
+                //     document.getElementById("newTotal").innerHTML = ``
+                //     document.getElementById("buttons").innerHTML = ``
+                // }
+                // function onCickOk() {
+                // }
+                document.getElementById("confirmation").innerHTML = `<h4 id="confirm-ask">add ${event.target.id} to your account?</h4><div id="confirm-buttons"><button id="no">no</button><button id="yes">yes</button></div>`;
                 document.getElementById("total").innerHTML = ``;
                 document.getElementById("newTotal").innerHTML = ``;
                 document.getElementById("buttons").innerHTML = ``;
                 document.getElementById("no").addEventListener("click", onClickNo)
                 document.getElementById("yes").addEventListener("click", onClickYes)
+                // document.getElementById("my-account").addEventListener("click", onClickMyAccount)
                 console.log(event);
             }
             let buttons = [`$${result.change}`,`$1.00`,`$3.00`,`Other`];
@@ -31,13 +42,14 @@ chrome.storage.sync.get(['total', 'totalInt', 'roundUp', 'change'], function(res
             button.innerHTML = buttons[i];
             button.setAttribute("id", buttons[i]);
             button.setAttribute("class", 'btn');
-            button.addEventListener("click", onClick)
+            button.addEventListener("click", onClickButton)
             document.getElementById("buttons").appendChild(button);
         }
     }
     calcTotals()
     createButtons()
 }
+
     
     frontPage();
 })
