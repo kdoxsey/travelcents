a2b0844406ccf547f448ad3fe2938c25b30e60ff
kdoxsey <kdoxse@gmail.com>
2021-04-20 10:56:20 -0400

    balance accumulates in a js alert when you click a button. I need to store the value because right now the value resets whenever you close out the window

--- popup.js
+++ popup.js
@@ -1,55 +1,51 @@
 chrome.storage.sync.get(['total', 'totalInt', 'roundUp', 'change'], function(result) {
-    let totalSaved = 0
 
-    function frontPage() {    
-        function calcTotals() { 
-            document.getElementById("total").innerHTML = `Checkout Total: ${result.total}`;
-            document.getElementById("newTotal").innerHTML = `New Checkout Total: $${result.roundUp}.00`;
+    let balance = parseFloat(0)
+    function homePage() {
+    // this is the page that is displayed when there is no checkout page detected
+        const homePageDiv = document.createElement("div")
+        document.createTextNode(homePageDiv)
+        document.getElementById('homePage').appendChild(homePageDiv)
+    }
+
+
+    function createButtons() {
+    // this is the part of the savePage that creates the buttons
+        function onClickButton(event) {
+        // this happens when a save button is clicked
+            balance += parseFloat(event.target.id)
+            alert(`My balance: $${balance}`)
         }
 
-        function createButtons() {
-            function onClickButton(event) {
-                function onClickNo() {
-                    frontPage();
-                    document.getElementById("confirmation").innerHTML = ``;
-                }
-                function onClickYes() {
-                    totalSaved += parseFloat(result.change);
-                    document.getElementById("confirmation").innerHTML = `<h4>You added ${event.target.id} to your account.</h4><button id="ok">Ok</button>`;
-                    console.log(totalSaved)
-                }
-
-                // function onClickMyAccount() {
-                //     document.getElementById("confirmation").innerHTML = `Total saved: $${totalSaved}`
-                //     document.getElementById("total").innerHTML = ``
-                //     document.getElementById("newTotal").innerHTML = ``
-                //     document.getElementById("buttons").innerHTML = ``
-                // }
-                // function onCickOk() {
-                // }
-                document.getElementById("confirmation").innerHTML = `<h4 id="confirm-ask">add ${event.target.id} to your account?</h4><div id="confirm-buttons"><button id="no">no</button><button id="yes">yes</button></div>`;
-                document.getElementById("total").innerHTML = ``;
-                document.getElementById("newTotal").innerHTML = ``;
-                document.getElementById("buttons").innerHTML = ``;
-                document.getElementById("no").addEventListener("click", onClickNo)
-                document.getElementById("yes").addEventListener("click", onClickYes)
-                // document.getElementById("my-account").addEventListener("click", onClickMyAccount)
-                console.log(event);
-            }
-            let buttons = [`$${result.change}`,`$1.00`,`$3.00`,`Other`];
-            for(i = 0; i < buttons.length; i++) {
+        let buttons = [parseFloat(result.change), 1, 3, `Other`];
+        for(i = 0; i < buttons.length; i++) {
             var button = document.createElement("button");
-            button.innerHTML = buttons[i];
+            if (typeof buttons[i] === "number" || typeof buttons[i] === "float") {
+                button.innerHTML = `$${parseFloat(buttons[i]).toFixed(2)}`;
+            } else {
+                button.innerHTML = `${buttons[i]}`
+            }
             button.setAttribute("id", buttons[i]);
             button.setAttribute("class", 'btn');
             button.addEventListener("click", onClickButton)
             document.getElementById("buttons").appendChild(button);
         }
+
     }
+
+    function calcTotals() {
+        // this is the part of the savePage that calculates the totals 
+        document.getElementById("total").innerHTML = `Checkout Total: ${result.total}`;
+        document.getElementById("newTotal").innerHTML = `New Checkout Total: $${result.roundUp}.00`;
+    }
+
+    function savePage() {    
+        // this is the page that is displayed when a checkout page is detected, asking you if you want to save your change or other amounts
     calcTotals()
     createButtons()
-}
+    }
 
-    
-    frontPage();
+    // This is what loads by default
+    savePage();
+    homePage()
 })
