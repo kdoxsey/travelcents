56582822d1d0ea1c8cd4974faaa0d29a5919dca1
kdoxsey <kdoxse@gmail.com>
2021-04-21 15:18:12 -0400

    main branch now up to date with dev

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
 // console.log('background running')
 chrome.browserAction.setBadgeText({text: "1"});
 chrome.browserAction.setBadgeBackgroundColor({color: "red"});
+
+console.log('background running')
