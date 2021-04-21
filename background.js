f81806a58607f96d26fbb54f33edea8264bed067
kdoxsey <kdoxse@gmail.com>
2021-04-21 14:22:29 -0400

    background script console logs the tab.id... still unable to use chrome messaging API

--- background.js
+++ background.js
@@ -1,24 +1,11 @@
-// console.log('background running')
+console.log('background running')
 // chrome.browserAction.setBadgeText({text: "1"});
 // chrome.browserAction.setBadgeBackgroundColor({color: "red"});
-
-
-
-// var currentURL;
-
-// chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT}, 
-// function(tabs){
-// 	getCurrentURL(tabs[0].url);
-// });
-
-// function getCurrentURL(tab){
-// 	currentURL = tab;
-// }
-// chrome.storage.sync.set({'currentURL': currentURL})
 
 chrome.browserAction.onClicked.addListener(buttonClicked);
 
 function buttonClicked(tab) {
+    console.log(tab.id)
     let msg = {
         txt: "hello"
     }
